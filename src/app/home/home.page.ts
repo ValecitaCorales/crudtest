348b423cff788e4ae351f5360f7e3590f84a226d
ValecitaCorales <va.corales@duocuc.cl>
2022-04-12 17:14:05 -0400

    subiendo el proyecto

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { DataService } from '../service/data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  users = [];
+
+  constructor(private dataService : DataService , private alertCtrl : AlertController) 
+  {
+    this.dataService.getUser().subscribe(res =>{
+      console.log(res);
+      this.users = res;
+    })
+  }
+  openNote(note){
+  }
+async addUser(){
+  const alert = await this.alertCtrl.create({
+    header :'Agrege Usuario',
+    inputs: [
+      {
+        name : 'nameC',
+        placeholder: 'Ingrese Nombre Completo',
+        type:'text'
+      },
+      {
+        name : 'rut',
+        placeholder: 'Ingrese Rut',
+        type:'text'
+      },
+      {
+        name : 'ocupacion',
+        placeholder: 'Ingrese Ocupacion',
+        type:'text'
+      },
+      {
+        name : 'mascota',
+        placeholder: 'Ingrese mascota',
+        type:'text'
+      }
+    ],
+    buttons:[
+      {
+        text : 'Cancelar',
+        role : 'cancel'
+      },
+      {
+        text: 'Agregar',
+        handler: (res) => {
+          this.dataService.addUser({nameC: res.nameC , rut : res.rut , ocupacion : res.ocupacion , mascota : res.mascota })
+        }
+      }
+    ]
+  });
+  await alert.present();
+}
+
+
+}
